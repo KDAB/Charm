5d3a903079231bb7f4380c279f8301b4bb0abb03
Mirko Boehm <mirko@kde.org>
2008-02-08 04:04:45 +0000

    Core does not link QtGui add directory for the timesheet processor tool

---
+++ Tools/TimesheetProcessor/main.cpp
@@ -0,0 +1,10 @@
+/* This program adds or removes timesheet XML files to and from an SQL
+ * database.
+ */
+#include <iostream>
+
+int main( int argc, char** argv )
+{
+    using namespace std;
+    cout << "Timesheet Processor" << endl;
+}
