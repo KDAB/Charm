c6bacf80f8105d012c7414896baef64a5673c715
Mirko Boehm <mirko@kde.org>
2008-06-23 07:51:26 +0000

    do not override CMAKE_INSTALL_PREFIX add a define if builds against a static Qt are enabled only link the static jpeg plugin if this variable is defined

--- Charm/Charm.cpp
+++ Charm/Charm.cpp
@@ -4,7 +4,9 @@
 #include "Core/CharmExceptions.h"
 #include "Application.h"
 
+#if defined CHARM_BUILD_STATIC_QT
 Q_IMPORT_PLUGIN(qjpeg)
+#endif
 
 int main ( int argc, char** argv )
 {
@@ -15,5 +17,7 @@ int main ( int argc, char** argv )
 		QMessageBox::critical( 0,
 				QObject::tr( "Charm Database Error"),
 				QObject::tr( e.what() ) );
+		return 1;
 	}
+	return 0;
 }
