30f0f153cbf2075e3277994768df432dd692bc57
David Faure <faure@kde.org>
2008-10-17 16:53:09 +0000

    Implement --version

--- Charm/Charm.cpp
+++ Charm/Charm.cpp
@@ -2,6 +2,7 @@
 #include <QtPlugin>
 
 #include "Application.h"
+#include "CharmVersion.h"
 
 #if defined CHARM_BUILD_STATIC_QT
 Q_IMPORT_PLUGIN(qjpeg)
@@ -9,6 +10,10 @@ Q_IMPORT_PLUGIN(qjpeg)
 
 int main ( int argc, char** argv )
 {
+	if (argc == 2 && qstrcmp(argv[1], "--version") == 0) {
+		printf("Charm version " CHARM_VERSION "\n");
+		return 0;
+	}
 	Application app ( argc,  argv );
 	return app.exec();
 }
---
+++ Charm/CharmVersion.h
@@ -0,0 +1,6 @@
+#ifndef CHARM_VERSION_H
+#define CHARM_VERSION_H
+
+#define CHARM_VERSION "1.3 beta"
+
+#endif
