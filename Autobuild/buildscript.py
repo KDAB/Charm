0232230074a5d81fe1e59d5092b6772bf6a4bf00
Sebastian Sauer <mail@dipe.org>
2009-02-09 10:07:14 +0000

    adopt to changes in the buildsys

--- Autobuild/buildscript.py
+++ Autobuild/buildscript.py
@@ -17,17 +17,18 @@ def preBuildJobHook( buildJob ):
 Debug = Configuration( Product, 'Debug' )
 Debug.setPreBuildHook( preBuildJobHook )  
 Debug.setBuilder( 'cmake' )
-Debug.addPackageDependency( 'Qt-4.[4-9].?' )
+Debug.addPackageDependency( 'Qt-4.[4-9].?-Shared-Debug' )
 Debug.addBlacklistedPlatform( 'win32-msvc' ) # disable MSVC6
 Debug.setOptions( '-D CMAKE_BUILD_TYPE=debug -D CHARM_ENABLE_TOOLS_BUILD:BOOL=true' )
 
 Release = copy.copy( Debug, )
 Release.setConfigName( 'Release' )
+Release.setPackageDependencies( [ 'Qt-4.[4-9].?-Shared-Release' ] )
 Release.setOptions( '-D CMAKE_BUILD_TYPE=release -D CHARM_ENABLE_TOOLS_BUILD:BOOL=true' )
 
 StaticRelease = copy.copy( Debug )
 StaticRelease.setConfigName( 'Static Release' )
-StaticRelease.setPackageDependencies( [ 'Qt-4.[4-9].?-Static' ] )
+StaticRelease.setPackageDependencies( [ 'Qt-4.[4-9].?-Static-Release' ] )
 StaticRelease.setOptions( '-D CMAKE_BUILD_TYPE=release -D CHARM_BUILD_STATIC_QT:BOOL=true' )
 
 # add a callback to manipulate the package location depending on the branch: 
