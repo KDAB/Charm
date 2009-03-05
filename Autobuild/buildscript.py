754cc054e0fc9212a7d1464dca50de55485dce50
Mirko Boehm <mirko@kde.org>
2009-03-05 23:09:51 +0000

    use simpler build script in trunk as well

--- Autobuild/buildscript.py
+++ Autobuild/buildscript.py
@@ -2,27 +2,24 @@
 import copy, os, platform
 from AutobuildCore.Configuration import Configuration
 from AutobuildCore.Project import Project
+from AutobuildCore.Callbacks.DynamicUploadLocation import BranchNamePackageSubdir
+from AutobuildCore.Callbacks.ConfigurationBuildSequence import ConfigurationBuildSequence
 
 scmPath = 'svn://kde.codeyard.cs.ru.nl/home/kde/trunk/playground/utils/charm'
 Product = Project( 'Charm', '0.4.2' )
 Product.setScmUrl( scmPath + '/trunk' )
 Product.setScmSrcDir( '' )
+Product.addCallback( BranchNamePackageSubdir( scmPath ) )
 
-# add a callback to manipulate the configuration build sequence at runtime:
-def preBuildJobHook( buildJob ):
-	if 'Static' not in buildJob.configuration().getConfigName():
-		step = buildJob.executomat().step( 'conf-bin-package' )
-		step.setEnabled( False ) # only package up static builds
-
-Debug = Configuration( Product, 'Debug' )
-Debug.setPreBuildHook( preBuildJobHook )  
+Debug = Configuration( Product, 'Shared Debug' )
+Debug.addCallback( ConfigurationBuildSequence( 'conf-bin-package', '*Static*', False, False ) ) # do not package shared builds
 Debug.setBuilder( 'cmake' )
 Debug.addPackageDependency( 'Qt-4.[4-9].?-Shared-Debug' )
 Debug.addBlacklistedPlatform( 'win32-msvc' ) # disable MSVC6
 Debug.setOptions( '-D CMAKE_BUILD_TYPE=debug -D CHARM_ENABLE_TOOLS_BUILD:BOOL=true' )
 
 Release = copy.copy( Debug, )
-Release.setConfigName( 'Release' )
+Release.setConfigName( 'Shared Release' )
 Release.setPackageDependencies( [ 'Qt-4.[4-9].?-Shared-Release' ] )
 Release.setOptions( '-D CMAKE_BUILD_TYPE=release -D CHARM_ENABLE_TOOLS_BUILD:BOOL=true' )
 
@@ -30,11 +27,5 @@ StaticRelease = copy.copy( Debug )
 StaticRelease.setConfigName( 'Static Release' )
 StaticRelease.setPackageDependencies( [ 'Qt-4.[4-9].?-Static-Release' ] )
 StaticRelease.setOptions( '-D CMAKE_BUILD_TYPE=release -D CHARM_BUILD_STATIC_QT:BOOL=true' )
-
-# add a callback to manipulate the package location depending on the branch: 
-def preSetupCallback( project ):
-	if len( scmPath ) < len( project.getScmUrl() ):
-		project.setPackageSubDir(project.getScmUrl()[len( scmPath ):])
-Product.registerPresetupCallback(preSetupCallback)
 
 Product.build( [ Debug, Release, StaticRelease] )
