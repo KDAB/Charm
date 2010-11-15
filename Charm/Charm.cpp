c6d8d9b0c42bdbbacd8f1ecc2e0b39c9151cf156
Mirko Boehm <mirko.boehm@kdab.com>
2010-11-15 13:13:29 +0100

    Add an exception handler around the call to exec. Yes, exceptions should not leak from the event loop, but it might happen. In that case, make sure to produce a reasonable message to the user.

--- Charm/Charm.cpp
+++ Charm/Charm.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <QFile>
 #include <QMessageBox>
 #include <QtPlugin>
@@ -5,11 +7,13 @@
 #include <QString>
 
 #include "Application.h"
+#include "Core/CharmExceptions.h"
 
 int main ( int argc, char** argv )
 {
     if (argc == 2 && qstrcmp(argv[1], "--version") == 0) {
-        printf("Charm version " CHARM_VERSION "\n");
+        using namespace std;
+        cout << "Charm version " << CHARM_VERSION << endl;
         return 0;
     }
 
@@ -25,5 +29,15 @@ int main ( int argc, char** argv )
     }
 
     Application app ( argc,  argv );
-    return app.exec();
+
+    try {
+        return app.exec();
+    } catch( CharmException& e ) {
+        const QString msg( QObject::tr( "An application exception has occured. Charm will be terminated. The error message was:\n"
+                                       "%1\n"
+                                       "Please report this as a bug at https://quality.kdab.com/browse/CHM." ).arg( e.what() ) );
+        QMessageBox::critical( 0, QObject::tr( "Application Error" ), msg );
+        using namespace std;
+        cerr << qPrintable( msg ) << endl;
+    }
 }
