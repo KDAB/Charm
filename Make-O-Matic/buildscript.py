5867f5d99e47ede62b44b8503c737075d18136b1
Mirko Boehm <mirko@kdab.com>
2010-12-07 16:53:12 +0100

    Don't use build in the build name ;-)

--- Make-O-Matic/buildscript.py
+++ Make-O-Matic/buildscript.py
@@ -18,8 +18,7 @@ from core.environments.Environments import Environments
 from core.plugins.DoxygenGenerator import DoxygenGenerator
 import socket
 
-build, project = BuildProject( build = 'Charm Build', name = 'Charm',
-	version = '1.4.0', url = 'git://github.com/KDAB/Charm.git' )
+build, project = BuildProject( name = 'Charm', version = '1.4.0', url = 'git://github.com/KDAB/Charm.git' )
 
 sharedDebug = Environments( [ 'Qt-4.[67].?-Shared-Debug' ], 'Qt 4 Shared Debug', project )
 debug = Configuration( 'Debug', sharedDebug, )
