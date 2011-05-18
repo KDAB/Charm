8d2a037ee15b001ed2f069ed64c9d4c1860b9442
Mike McQuaid <mike.mcquaid@kdab.com>
2011-05-18 15:31:18 +0100

    Update DeployQt4 module.

--- CMake/DeployQt4.cmake
+++ CMake/DeployQt4.cmake
@@ -7,11 +7,11 @@
 #   install_qt4_app
 # Requires CMake 2.6 or greater and depends on BundleUtilities.cmake.
 #
-#  FIXUP_QT4_BUNDLE(<app> <qtplugins> [<libs> <dirs>])
+#  FIXUP_QT4_BUNDLE(<app> <qtplugins> [<libs> <dirs> <plugins_dir>])
 # Copies Qt plugins, writes a Qt configuration file (if needed) and fixes up a
 # Qt4 executable using BundleUtilities.
 #
-#  INSTALL_QT4_APP(<executable> <qtplugins> [<libs> <dirs>])
+#  INSTALL_QT4_APP(<executable> <qtplugins> [<libs> <dirs> <plugins_dir>])
 # Installs plugins, writes a Qt configuration file (if needed) and fixes up a
 # Qt4 executable using BundleUtilties.
 
@@ -35,20 +35,31 @@ include(BundleUtilities)
 function(fixup_qt4_bundle app qtplugins)
     set(libs ${ARGV2})
     set(dirs ${ARGV3})
+    set(plugins_dir ${ARGV4})
 
     message(STATUS "fixup_qt4_bundle")
     message(STATUS "  app='${app}'")
     message(STATUS "  qtplugins='${qtplugins}'")
     message(STATUS "  libs='${libs}'")
     message(STATUS "  dirs='${dirs}'")
+    message(STATUS "  plugins_dir='${plugins_dir}'")
+
+    if(QT_LIBRARY_DIR)
+        list(APPEND dirs "${QT_LIBRARY_DIR}")
+    endif()
 
     if(APPLE)
-        set(plugins_dir "PlugIns")
+        if(NOT plugins_dir)
+            set(plugins_dir "PlugIns")
+        endif()
         set(plugins_path "${app}/Contents/${plugins_dir}")
         set(qt_conf_dir "${app}/Contents/Resources")
         set(write_qt_conf 1)
     else()
-        set(plugins_dir "plugins")
+        if(NOT plugins_dir)
+            set(plugins_dir "plugins")
+        endif()
+        set(plugins_dir ${plugin_inst_dir})
         get_filename_component(app_path "${app}" PATH)
         set(plugins_path "${app_path}/${plugins_dir}")
         set(qt_conf_dir "")
@@ -94,9 +105,14 @@ endfunction(fixup_qt4_bundle)
 function(install_qt4_app executable qtplugins)
     set(libs ${ARGV2})
     set(dirs ${ARGV3})
+    set(plugins_dir ${ARGV4})
+    if(QT_LIBRARY_DIR)
+        list(APPEND dirs "${QT_LIBRARY_DIR}")
+    endif()
+
     install(CODE
 " INCLUDE( \"${CMAKE_MODULE_PATH}/DeployQt4.cmake\" )
   SET( BU_CHMOD_BUNDLE_ITEMS TRUE )
-  FIXUP_QT4_BUNDLE( \"\${CMAKE_INSTALL_PREFIX}/${executable}\" \"${qtplugins}\" \"${libs}\" \"${dirs}\" ) "
+  FIXUP_QT4_BUNDLE( \"\${CMAKE_INSTALL_PREFIX}/${executable}\" \"${qtplugins}\" \"${libs}\" \"${dirs}\" \"${plugins_dir}\" ) "
     )
 endfunction(install_qt4_app)
