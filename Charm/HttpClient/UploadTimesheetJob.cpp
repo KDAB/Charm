175751bafe8dd4d6c07ae7ad18388c36eee7e336
Guillermo A. Amaral <gamaral@kdab.com>
2014-04-07 08:40:37 -0700

    QRegExp needs to be included in Qt 4
    Change-Id: Id894c759bb94d871b05483e3aa41c5330ffe45fb
    Reviewed-on: https://codereview.kdab.com/8567
    Reviewed-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Frank Osterfeld <frank.osterfeld@kdab.com>

--- Charm/HttpClient/UploadTimesheetJob.cpp
+++ Charm/HttpClient/UploadTimesheetJob.cpp
@@ -3,6 +3,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QNetworkRequest>
+#include <QRegExp> // Required for Qt 4
 #include <QSettings>
 
 UploadTimesheetJob::UploadTimesheetJob(QObject* parent)
