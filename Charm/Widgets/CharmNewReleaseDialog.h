30c82499cfddcd126c55215bcdfa01365b2b6246
Allen Winter <allen.winter@kdab.com>
2015-08-20 20:21:17 -0400

    add KDAB copyright and license header
    Change-Id: I225dc3c1d9b7ecab2ceddc01dc8d03684130183f

--- Charm/Widgets/CharmNewReleaseDialog.h
+++ Charm/Widgets/CharmNewReleaseDialog.h
@@ -1,3 +1,26 @@
+/*
+  CharmNewReleaseDialog.h
+
+  This file is part of Charm, a task-based time tracking application.
+
+  Copyright (C) 2015-2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+
+  Author: Michel Boyer de la Giroday <michel.giroday@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef CHARMNEWRELEASEDIALOG
 #define CHARMNEWRELEASEDIALOG
 
@@ -36,4 +59,3 @@ private:
 };
 
 #endif // CHARMNEWRELEASEDIALOG
-
