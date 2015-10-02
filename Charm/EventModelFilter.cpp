KDAB/Charm

commit 510230334a3903dfdb5c130b90c719f8eac5b92f

EventModelFilter improvements
Call invalidateFilter, make it possible to filter by task ID, respect
QSortFilterProxyModel filter options.

Change-Id: Ib6f48f632a164a5010766418bcb6940c9a2a293c
Reviewed-on: https://codereview.kdab.com/24887
Tested-by: Continuous Integration <build@kdab.com>
Reviewed-by: Boyer de la Giroday Michel <michel.giroday@kdab.com>
Reviewed-by: Frank Osterfeld <frank.osterfeld@kdab.com>

Charm/EventModelFilter.cpp
@@ -27,6 +27,7 @@
 EventModelFilter::EventModelFilter( CharmDataModel* model, QObject* parent )
     : QSortFilterProxyModel( parent )
     , m_model( model )
+    , m_filterId()
 {
     setSourceModel( &m_model );
     setDynamicSortFilter( true );
@@ -73,21 +74,49 @@ QModelIndex EventModelFilter::indexForEvent( const Event& event ) const
 
 bool EventModelFilter::filterAcceptsRow( int srow, const QModelIndex& sparent ) const
 {
+    if ( QSortFilterProxyModel::filterAcceptsRow( srow, sparent ) == false ) {
+        return false;
+    }
+
     const Event& event = m_model.eventForIndex( m_model.index( srow, 0, sparent ) );
-    return ( event.startDateTime().date() >= m_start
-             && event.startDateTime().date() < m_end );
+
+    if ( m_filterId != TaskId() && event.taskId() != m_filterId ) {
+        return false;
+    }
+
+    if ( m_start.isValid() && event.startDateTime().date() < m_start ) {
+        return false;
+    }
+
+    if ( m_end.isValid() && event.endDateTime().date() >= m_end ) {
+        return false;
+    }
+
+    return true;
 }
 
 void EventModelFilter::setFilterStartDate( const QDate& date )
 {
+    if ( m_start == date )
+        return;
     m_start = date;
-    filterChanged();
+    invalidateFilter();
 }
 
 void EventModelFilter::setFilterEndDate( const QDate& date )
 {
+    if ( m_end == date )
+        return;
     m_end = date;
-    filterChanged();
+    invalidateFilter();
+}
+
+void EventModelFilter::setFilterTaskId( TaskId id )
+{
+    if ( m_filterId == id )
+        return;
+    m_filterId = id;
+    invalidateFilter();
 }
 
 int EventModelFilter::totalDuration() const

Charm/EventModelFilter.h
@@ -56,6 +56,7 @@ public:
 
     void setFilterStartDate( const QDate& date );
     void setFilterEndDate( const QDate& date );
+    void setFilterTaskId( TaskId id );
 
     // implement CommandEmitterInterface:
     void commitCommand( CharmCommand* ) override;
@@ -71,6 +72,7 @@ private:
     EventModelAdapter m_model;
     QDate m_start;
     QDate m_end;
+    TaskId m_filterId;
 };
 
 #endif

commit 3bfa59c8d1fb30abee7f93d2144870c548e4c69f

Fix event filtering
Start date is to be used here, not start and end date. Makes events
that start yesterday and end today show up under "yesterday" in the
event editor.

This was a regression introduced in 510230334a.

Change-Id: I70d1c5aef1b59bd857f9dc1efb0581ca5c5a72d9
Reviewed-on: https://codereview.kdab.com/25946
Tested-by: Continuous Integration <build@kdab.com>
Reviewed-by: Boyer de la Giroday Michel <michel.giroday@kdab.com>
Reviewed-by: Frank Osterfeld <frank.osterfeld@kdab.com>

Charm/EventModelFilter.cpp
@@ -84,11 +84,12 @@ bool EventModelFilter::filterAcceptsRow( int srow, const QModelIndex& sparent ) 
         return false;
     }
 
-    if ( m_start.isValid() && event.startDateTime().date() < m_start ) {
+    const auto startDate = event.startDateTime().date();
+    if ( m_start.isValid() && startDate < m_start ) {
         return false;
     }
 
-    if ( m_end.isValid() && event.endDateTime().date() >= m_end ) {
+    if ( m_end.isValid() && startDate >= m_end ) {
         return false;
     }
 