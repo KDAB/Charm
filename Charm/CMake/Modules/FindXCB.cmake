cc6ce0f1c640e7fb04f098586bdf11a08ade86b6
Allen Winter <allen.winter@kdab.com>
2016-05-16 12:50:26 -0400

    CMake/Modules/FindXCB.cmake - relicense to GPLv2+ ISSUE: #278
    Change-Id: Iaf0b2ccc300714f582efcfe6f0573f1148798075

--- Charm/CMake/Modules/FindXCB.cmake
+++ Charm/CMake/Modules/FindXCB.cmake
@@ -1,6 +1,18 @@
 # Copyright (C) 2015-2016 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
 
-# Redistribution and use is allowed according to the terms of the BSD license
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 #
 # Try to find XCB library and include path.
