98e38ed95c4f244f70a414b237655f6748d13629
Allen Winter <allen.winter@kdab.com>
2017-01-05 16:27:17 -0500

    update Copyright year
    Change-Id: Ib23225b51f4492f125fa103065bbbb8924daa3b1

--- Charm/Idle/X11IdleDetector.h
+++ Charm/Idle/X11IdleDetector.h
@@ -3,7 +3,7 @@
 
   This file is part of Charm, a task-based time tracking application.
 
-  Copyright (C) 2008-2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Copyright (C) 2008-2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
 
   Author: Jesper Pedersen <jesper.pedersen@kdab.com>
   Author: Frank Osterfeld <frank.osterfeld@kdab.com>
